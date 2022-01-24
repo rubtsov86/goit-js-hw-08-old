f72edd52ccf988f6900168df0a74f6bfeda6f500
Serhii Rubtsov <rubtsov1986@bigmir.net>
2022-01-24 20:03:56 +0200

    finish

--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,19 +1,30 @@
 import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'inputData';
+
 const formRef = document.querySelector('form');
 const inputRef = document.querySelector('input');
 const textareaRef = document.querySelector('textarea');
 
+if (localStorage.getItem(STORAGE_KEY)) {
+    inputRef.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).email;
+    textareaRef.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).message;
+}
+
 formRef.addEventListener('submit', onFormSubmit);
+textareaRef.addEventListener('input', throttle(onTextareaInput, 500));
+
+
 
 function onFormSubmit(e) {
     e.preventDefault();
-    
-
-    console.log(inputRef.value);
-
-    localStorage.setItem('inputData', JSON.stringify({"email": inputRef.value, "message": textareaRef.value }))
-    console.log(localStorage.getItem('inputData'));
     e.target.reset();
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)))
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(e) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({ "email": inputRef.value, "message": textareaRef.value }));
+    // console.log(localStorage.getItem('inputData'));
 }
 
